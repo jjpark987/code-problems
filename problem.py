e174abf65e1eb112540f5d60c91fe1f1973f6cc7
Jason Park <jjpark987@gmail.com>
2024-05-09 11:00:43 -0700

    maximum_happiness_sum

--- problem.py
+++ problem.py
@@ -6,24 +6,65 @@ from typing import List, Optional
 # from collections import deque, defaultdict
 
 class Solution:
-    def findRelativeRanks(self, score: List[int]) -> List[str]:
-        sorted_score = sorted(score, reverse=True)
-        medals = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
-        rank_map = {s: medals[i] if i < 3 else str(i + 1) for i, s in enumerate(sorted_score)}
-        # for i, s in enumerate(sorted_score):
-        #     match i:
-        #         case 0:
-        #             rank_map[s] = 'Gold Medal'
-        #         case 1:
-        #             rank_map[s] = 'Silver Medal'
-        #         case 2:
-        #             rank_map[s] = 'Bronze Medal'
-        #         case _:
-        #             rank_map[s] = str(i + 1)
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        total = 0
+        turns = 0
+        
+        for i in range(k):
+            if happiness[i] - turns <= 0:
+                return total
 
-        result = [rank_map[score[i]] for i in range(len(score))]
+            total += happiness[i] - turns
 
-        return result
-    
+            turns += 1
 
-print(Solution().findRelativeRanks([10,3,8,9,4]))
+        return total
+
+# print(Solution().maximumHappinessSum([1,2,3], 2))
+# print(Solution().maximumHappinessSum([1,1,1,1], 2))
+# print(Solution().maximumHappinessSum([2,3,4,5], 1))
+# print(Solution().maximumHappinessSum([12,1,42], 3))
+
+"""
+You are given an array happiness of length n, and a positive integer k.
+
+There are n children standing in a queue, where the ith child has happiness value happiness[i]. You want to select k children from these n children in k turns.
+
+In each turn, when you select a child, the happiness value of all the children that have not been selected till now decreases by 1. Note that the happiness value cannot become negative and gets decremented only if it is positive.
+
+Return the maximum sum of the happiness values of the selected children you can achieve by selecting k children.
+
+ 
+
+Example 1:
+
+Input: happiness = [1,2,3], k = 2
+Output: 4
+Explanation: We can pick 2 children in the following way:
+- Pick the child with the happiness value == 3. The happiness value of the remaining children becomes [0,1].
+- Pick the child with the happiness value == 1. The happiness value of the remaining child becomes [0]. Note that the happiness value cannot become less than 0.
+The sum of the happiness values of the selected children is 3 + 1 = 4.
+Example 2:
+
+Input: happiness = [1,1,1,1], k = 2
+Output: 1
+Explanation: We can pick 2 children in the following way:
+- Pick any child with the happiness value == 1. The happiness value of the remaining children becomes [0,0,0].
+- Pick the child with the happiness value == 0. The happiness value of the remaining child becomes [0,0].
+The sum of the happiness values of the selected children is 1 + 0 = 1.
+Example 3:
+
+Input: happiness = [2,3,4,5], k = 1
+Output: 5
+Explanation: We can pick 1 child in the following way:
+- Pick the child with the happiness value == 5. The happiness value of the remaining children becomes [1,2,3].
+The sum of the happiness values of the selected children is 5.
+ 
+
+Constraints:
+
+1 <= n == happiness.length <= 2 * 105
+1 <= happiness[i] <= 108
+1 <= k <= n
+"""
