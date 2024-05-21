a9a6a19776849aaf73f05b79b0189473c6036067
Jason Park <jjpark987@gmail.com>
2024-05-21 15:52:34 -0700

    subsets

--- solutions/manipulations/bitwise/subsets.py
+++ solutions/manipulations/bitwise/subsets.py
@@ -9,33 +9,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
 
-        # iterate range(2**len(nums)) times, each one representing a subset
         for i in range(2**len(nums)):
             subset = []
 
-            # to find each subset, we iterate through the list of ints
             for j in range(len(nums)):
-                # starting from j = 0 (the right most bit), we shift the bits to the right
-                # this allows us to compare the jth bit of bin(i) because it is now in the right most position
-                # we check if the bit in the right most position is 1
-                # if it is, we add the element in index j into the subset
                 if (i >> j) & 1:
                     subset.append(nums[j])
 
             result.append(subset)
 
         return result
-    
-        # RECURSIVE BACKTRACKING
-
-        # def backtrack(start, path):
-        #     result.append(path)
-        #     for i in range(start, len(nums)):
-        #         backtrack(i + 1, path + [nums[i]])
-
-        # result = []
-        # backtrack(0, [])
-        # return result
 
 
 print(Solution().subsets([1,2,3]))
