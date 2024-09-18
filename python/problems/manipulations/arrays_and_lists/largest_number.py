bf0659c60f9ff1688314355e5e9eeb0c2f03eebe
Jason Park <jjpark987@gmail.com>
2024-09-18 12:11:26 -0700

    largest_number update

--- python/problems/manipulations/arrays_and_lists/largest_number.py
+++ python/problems/manipulations/arrays_and_lists/largest_number.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
 from typing import List
+from functools import cmp_to_key
 
 # time: O(n*log(n)), space: O(n)
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        def compare(x, y):
+            # Compare concatenated strings
+            if x + y > y + x:
+                return -1  # x should come before y
+            elif x + y < y + x:
+                return 1   # y should come before x
+            else:
+                return 0   # equal
+            
+        # Turn all integers into strings
         for i in range(len(nums)):
             nums[i] = str(nums[i])
 
-        nums.sort(key=lambda x: int(x[0]), reverse= True)
+        # Sort the list using custom function
+        nums.sort(key=cmp_to_key(compare))
 
+        # Concatenate the strings
         result = ''
-        
         for n in nums:
             result += str(n)
 
+        # If all values are 0s, return '0'
+        if result[0] == '0':
+            return '0'
+
         return result
 
-print(Solution().largestNumber(nums = [10,2]))
-print('210')
-print(Solution().largestNumber(nums = [3,30,34,5,9]))
-print('9534330')
+# print(Solution().largestNumber(nums = [10,2]))
+# print('210')
+# print(Solution().largestNumber(nums = [3,30,34,5,9]))
+# print('9534330')
+print(Solution().largestNumber(nums = [0,0]))
+print('0')
 
 """
 category: manipulations
